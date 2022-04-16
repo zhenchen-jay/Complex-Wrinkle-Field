f50fd4a319a6b90a69a85c79cb8a1732e1c6eb00
CSYZ_ZKD_CZ <csyzzkdcz@gmail.com>
2022-04-16 17:42:02 -0500

    some clean up
    Former-commit-id: 607fa099a3b7c8f107066245d5689a8880255e7b [formerly 0b22f4f5dcea9f6fffcdb85ca83c008cd75c2d0a]
    Former-commit-id: a7f0588e377e4ad528705588037dc5cab5e2931b [formerly 7549ddadc777e07aa7455e869e7a0efb717c2b88] [formerly 41bcf7a19c0cff3e833904fab3f00f269bfdeef1 [formerly a18ee90f4d95fcec7353a1b35abb120f528f8a71]]
    Former-commit-id: 8447b0b69c9e6bdbf7d1bab017ed63d1c9fa5e39 [formerly ae3d48d6d2dddf0f1a56bdb7d409e4e112e7760b]
    Former-commit-id: 6dfffeff78a0327e990324aedaa28155f19cd3c5
    Former-commit-id: 9196af7b8d61b7e02dbec126a60e4931c6e5944a

--- include/MeshLib/MeshUpsampling.h
+++ include/MeshLib/MeshUpsampling.h
@@ -7,5 +7,3 @@
 void meshUpSampling(Eigen::MatrixXd V, Eigen::MatrixXi F, Eigen::MatrixXd &NV, Eigen::MatrixXi &NF, int numSubdivs, Eigen::SparseMatrix<double> *mat = NULL, std::vector<int>* facemap = NULL, std::vector<std::pair<int, Eigen::Vector3d>> *bary = NULL);
 
 void loopUpsampling(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::MatrixXd& NV, Eigen::MatrixXi& NF, int upsampledTimes, Eigen::SparseMatrix<double> *loopMat = NULL);
-
-void subdivideWrinkleFields(const Mesh& mesh, const Eigen::VectorXd& omega, const std::vector<std::complex<double>>& zvals, int level, Mesh& subMesh, Eigen::VectorXd& upOmega, std::vector<std::complex<double>>& upZvals);
