a682921718547e2e9078696e10d0d8844ca69553
ZhenChen <csyzzkdcz@gmail.com>
2022-12-26 14:43:59 -0600

    copy images script

--- pythonScript/copyFiles.py
+++ pythonScript/copyFiles.py
@@ -46,6 +46,22 @@ def copyFilesInFolderNew(source : str, target : str):
          for file in files:
             shutil.copy(os.path.join(modelFolder, file), os.path.join(outFolder, file))
 
+def copyImags(source : str, target : str):
+   allModelFolders = [os.path.join(source, o) for o in os.listdir(source) if os.path.isfile(os.path.join(source, o))]
+   if not os.path.isdir(target):
+      os.mkdir(target)
+   
+   for imgs in allModelFolders:
+      if imgs.find(".png") != -1:
+         imgName = os.path.split(imgs)[-1]
+         frameId = int(imgName.split("_")[-1].split(".")[0])
+         if frameId % 25 == 0:
+            print(imgName)
+            if os.path.exists(os.path.join(target, imgName)):
+               os.remove(os.path.join(target, imgName))
+            shutil.copy2(imgs, os.path.join(target, imgName))
+
 if __name__ == '__main__':
    # copyFilesInFolder('/media/zchen96/Extreme SSD/CWF_Dataset/paperResRerunNewFormula_final', '/media/zchen96/Extreme SSD/CWF_Dataset/paperResRerunNewFormula_finalVersion')
-   copyFilesInFolderNew('/media/zchen96/Extreme SSD/CWF_Dataset/keyFrameCheck', '/media/zchen96/Extreme SSD/CWF_Dataset/keyFrameCheck_finalVersion')
+   # copyFilesInFolderNew('/media/zchen96/Extreme SSD/CWF_Dataset/keyFrameCheck', '/media/zchen96/Extreme SSD/CWF_Dataset/keyFrameCheck_finalVersion')
+   copyImags('/media/zchen96/Extreme SSD/CWF_Dataset/paperResRerunNewFormula_final/bunny_localRotation/rendered_CWF', '/mnt/spinning1/zchen/WrinkleEdition_dataset/Kinematic-Wrinkle-Edition-with-Complex-Representation/Figs/imag/bunnyLocalRotation')
