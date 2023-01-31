634f17229bb28e29bef9602eaf2c8ca77fae7ef8
ZhenChen <csyzzkdcz@gmail.com>
2023-01-30 18:09:02 -0600

    remove the pythonscripts

--- pythonScript/CWFCommon.py
+++
@@ -1,2 +0,0 @@
-CWFDataFolder = "/mnt/spinning1/zchen/WrinkleEdition_dataset/paperResRerunNewFormula/"
-CWFEXEPath = "/home/zchen96/Projects/PhaseInterpolation_polyscope/build/bin/WrinkleInterpolationCli_bin"
--- pythonScript/OtherApproachesCommon.py
+++
@@ -1,2 +0,0 @@
-CWFDataFolder = "/mnt/spinning1/zchen/WrinkleEdition_dataset/paperResRerunSLERP/otherApproaches/"
-CWFEXEPath = "/home/zchen96/Projects/PhaseInterpolation_polyscope/build/bin/OtherApproachesCli_bin"
